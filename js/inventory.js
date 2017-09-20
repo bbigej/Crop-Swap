9e53ae1e5bfb56d4b932c16b23a2ab103ed00d1e
Bryan James Bigej <bbigej@gmail.com>
2017-09-19 18:13:55 -0700

    star and text renders to page

--- js/inventory.js
+++ js/inventory.js
@@ -1,29 +1,30 @@
 var produceArray = ["Carrots", "Apples", "Kale", "Strawberries", "Squash"];
 
+//array of locations of markets
 var locationsArray =[];
 
+//constructor function to build various locations
 var MarketLocation = function (neighborhood, address) {
     this.neighborhood = neighborhood;
     this.address = address;
 }
 
+//locations pushed into the array
 locationsArray.push(new MarketLocation("The Pearl", "123 Marshall St."));
 locationsArray.push(new MarketLocation("PSU", "1825 SW Broadway"));
 locationsArray.push(new MarketLocation("Sellwood", "789 SE Tacoma St."));
 locationsArray.push(new MarketLocation("Northeast", "2035 NE Alberta St"));
 locationsArray.push(new MarketLocation("Southeast","3203 SE Woodstock Blvd",));
 
-var produceLocationOptions = document.getElementsByClassName("produce-location-option");
-for (var index = 0; index < produceLocationOptions.length; index++) {
-produceLocationOptions.addEventListener("change", showLocations);
-}
 
+//code to add text to the paragraph
 var message = "blank";
 
 function updatePage() {
     document.getElementById("address-paragraph").innerHTML = message;
 }
 
+//code that adds stars to the map and updates the text on the page
 function showLocations() {
     if (userProfile[0].crops == produceArray.indexOf("Carrots")) {
         document.getElementById("star1").style.visibility = 'visible';
@@ -34,4 +35,12 @@ function showLocations() {
         updatePage();
     }
 
+}
+
+//event listener to add event listener to the drop down menu
+window.addEventListener("load", addListeners);
+
+function addListeners() {
+var produceLocationOptions = document.getElementById("produce-location-options");
+produceLocationOptions.addEventListener("change", showLocations);
 }
